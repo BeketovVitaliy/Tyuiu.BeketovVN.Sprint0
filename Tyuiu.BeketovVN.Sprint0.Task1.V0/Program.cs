f4fdb022e3de976d8523f476295813b93020f744
Avanor <avanor5242@yandex.ru>
2023-09-05 18:37:30 +0500

    Task1

---
+++ Tyuiu.BeketovVN.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BeketovVN.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string name = "Виталий";
+            int age = 21;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
