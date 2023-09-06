670d077e78ca0320dccdcf2a003dd7ec18c253e0
Avanor <avanor5242@yandex.ru>
2023-09-06 16:25:25 +0500

    Task2

---
+++ Tyuiu.BeketovVN.Sprint0.Test2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.Remoting;
+using System.Security.Cryptography.X509Certificates;
+using Tyuiu.BeketovVN.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.BeketovVN.Sprint0.Test2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMassegeValid()
+        {
+            var name = "Виталий";
+            var res = DataService.GetMassage(name);
+
+            Assert.AreEqual("привет, Виталий", res);
+        }
+    }
+}
---
+++ Tyuiu.BeketovVN.Sprint0.Test2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BeketovVN.Sprint0.Test2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BeketovVN.Sprint0.Test2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92a33ae9-601c-4174-af67-3c9810f2a8d1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
