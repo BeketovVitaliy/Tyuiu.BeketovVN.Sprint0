9795fbbf3ea7139f35ba1ea8d842e9af559a3d90
Avanor <avanor5242@yandex.ru>
2023-09-12 12:29:25 +0500

    Task4

---
+++ Tyuiu.BeketovVN.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BeketovVN.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.BeketovVN.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(5, 5));
+            Console.WriteLine(DataService.Subtraction(5, 5));
+            Console.WriteLine(DataService.Multiplication(5, 5));
+            Console.WriteLine(DataService.Division(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
